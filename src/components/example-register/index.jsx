7e0190a67d1ce2c660a042bfac3b5e72d947c23e
Bilal0410 <bilaln0410@gmail.com>
2023-10-10 17:15:58 +0200

    register form

--- src/components/example-register/index.jsx
+++ src/components/example-register/index.jsx
@@ -8,9 +8,69 @@ export default function ExampleRegisterPage() {
   const [isSuccess, setIsSuccess] = useState(false);
   const navigate = useNavigate();
 
+  const navigateToHome = () => {
+    setTimeout(() => {
+      navigate({ to: "/" });
+    }, 2000);
+  };
+
+  function validateEmail(email) {
+    const noroffEmailRegex = /^(?=.*@noroff\.no$|@stud\.noroff\.no$)/;
+    const isValid = noroffEmailRegex.test(email);
+    const errorMessage = document.getElementById("email-error-message");
+
+    if (!isValid) {
+      errorMessage.innerText =
+        "Make sure to either use @noroff.no / @stud.noroff.no";
+      document.getElementById("submit-button").disabled = true;
+    } else {
+      errorMessage.innerText = "";
+      document.getElementById("submit-button").disabled = false;
+    }
+  }
+
+  const handleOnSubmit = async (event) => {
+    event.preventDefault();
+
+    const { email, password, name, avatar, banner } = event.target.elements;
+
+    const payload = {
+      email: email.value,
+      password: password.value,
+      name: name.value,
+      avatar: avatar.value,
+      banner: banner.value,
+    };
+
+    try {
+      const res = await fetch(
+        "https://api.noroff.dev/api/v1/social/auth/login",
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+
+      const data = await res.json();
+      localStorage.setItem("access_token", data.accessToken);
+      setData(data);
+      setIsSuccess(res.ok);
+
+      navigateToHome();
+    } catch (error) {
+      console.warn("An error occurred", error);
+
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <>
-      <h1>Register Page</h1>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
@@ -19,7 +79,7 @@ export default function ExampleRegisterPage() {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
-            Sign in to your account
+            Register your account here
           </h2>
         </div>
 
@@ -30,6 +90,61 @@ export default function ExampleRegisterPage() {
             method="POST"
             onSubmit={handleOnSubmit}
           >
+            <div>
+              <label
+                htmlFor="name"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Avatar
+              </label>
+              <div className="mt-2">
+                <input
+                  id="avatar"
+                  name="avatar"
+                  type="file"
+                  accept="image/*"
+                  className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label
+                htmlFor="banner"
+                className="block text-sm font-medium leading-6 text-white"
+              >
+                Banner
+              </label>
+              <div className="mt-2">
+                <input
+                  id="banner"
+                  name="banner"
+                  type="file"
+                  accept="image/*"
+                  className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label
                 htmlFor="email"
@@ -44,8 +159,13 @@ export default function ExampleRegisterPage() {
                   type="email"
                   autoComplete="email"
                   required
+                  onInput={(e) => validateEmail(e.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+                <p
+                  id="email-error-message"
+                  className="text-sm text-red-700 mt-1"
+                ></p>
               </div>
             </div>
 
@@ -57,14 +177,7 @@ export default function ExampleRegisterPage() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
+                <div className="text-sm"></div>
               </div>
               <div className="mt-2">
                 <input
@@ -80,24 +193,15 @@ export default function ExampleRegisterPage() {
 
             <div>
               <button
+                id="submit-button"
                 disabled={isLoading}
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                {isLoading ? "signing in" : "Sign in"}
+                {isLoading ? "Signing in" : "Sign up"}
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-            >
-              Start a 14 day free trial
-            </a>
-          </p>
         </div>
       </div>
     </>
