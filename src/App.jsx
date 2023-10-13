3d442d4481fb6903f7d58b0dcba39dc9d210c9c9
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-10-13 16:40:44 +0200

    linked profiles to profilepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "@tanstack/react-router";
 import Navbar from "./components/navbar";
+import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ExampleProfiles from "./components/example-profiles";
+import ProfileDetail from "./components/example-profiledetail";
 import "./App.css";
 
 function App() {
+  const [profiles, setProfiles] = useState([]);
   return (
     <>
       <header>
@@ -10,7 +16,17 @@ function App() {
       </header>
 
       <main>
+        <Router>
         <Outlet />
+          <Switch>
+            <Route path="/profiles" exact>
+              <ExampleProfiles profiles={profiles} setProfiles={setProfiles} />
+            </Route>
+            <Route path="/profile/:name">
+              <ProfileDetail />
+            </Route>
+          </Switch>
+        </Router>
       </main>
 
       <footer>
