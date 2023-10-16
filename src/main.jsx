7ad52c33ecfd94f4026dc8080f59b0785a103f98
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-10-16 23:29:18 +0200

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Outlet } from "@tanstack/react-router";
-import Navbar from "./components/navbar";
-import "./App.css";
-import HomePage from "./pages/Home";
-import fetchSpecific from "./pages/Post";
-import ExampleProfiles from "./components/example-profiles";
-import ProfileDetail from "./components/example-profiledetail";
-import Post from "./components/example-posts";
+import ReactDOM from "react-dom/client";
+import { router } from "./root.js";
+import { RouterProvider } from "@tanstack/react-router";
+import "./index.css";
 
-function App() {
-  return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-
-      <main>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route path="/post" component={Post} />
-            <Route path="/post/:postId" component={fetchSpecific} />
-            <Route path="/profiles" exact component={ExampleProfiles} />
-            <Route path="/profile/:name" component={ProfileDetail} />
-          </Switch>
-          <Outlet />
-        </Router>
-      </main>
-
-      <footer>
-        <small>Created with ❤️ by You</small>
-      </footer>
-    </>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    {/* NOTE: We need to wrap our react app in the tanstack router */}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
