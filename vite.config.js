f04a0ec9cb0777702f6f819b1892d6a06603e351
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-10-16 23:17:16 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        home: resolve(__dirname, "home.jsx"),
-        profiles: resolve(__dirname, "profiles.jsx"),
-        login: resolve(__dirname, "login.jsx"),
-        plugins: [react()],
-      }
-    }
-  }
+  plugins: [react()],
 })
