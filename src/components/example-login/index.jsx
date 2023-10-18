cf53a4f7da1bc77f5c606153ba5ac6620b97ed21
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2023-10-18 14:16:06 +0200

    created edit and delete button for profile page

--- src/components/example-login/index.jsx
+++ src/components/example-login/index.jsx
@@ -66,7 +66,7 @@ export default function LoginForm() {
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
             alt="Your Company"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-black">
             Sign in to your account
           </h2>
         </div>
@@ -92,7 +92,7 @@ export default function LoginForm() {
                   type="email"
                   autoComplete="email"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
@@ -101,7 +101,7 @@ export default function LoginForm() {
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-white"
+                  className="block text-sm font-medium leading-6 text-black"
                 >
                   Password
                 </label>
--- src/components/myprofiledetail/index.jsx
+++ src/components/myprofiledetail/index.jsx
@@ -1,28 +1,34 @@
 import { useEffect, useState } from "react";
-import { useSearch } from "@tanstack/react-router";
 
-export default function ProfileDetail() {
+export default function MyProfileDetail() {
   const [profile, setProfile] = useState(null);
   const [posts, setPosts] = useState([]);
   const apiKey = localStorage.getItem("access_token");
-  const profileid = localStorage.getItem("user_name")
+  const profileid = localStorage.getItem("user_name");
+  const [editingPost, setEditingPost] = useState(null);
 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
-        const profileResponse = await fetch(`https://api.noroff.dev/api/v1/social/profiles/${profileid}`, {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        });
+        const profileResponse = await fetch(
+          `https://api.noroff.dev/api/v1/social/profiles/${profileid}`,
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+            },
+          }
+        );
         const profileData = await profileResponse.json();
         setProfile(profileData);
 
-        const postsResponse = await fetch(`https://api.noroff.dev/api/v1/social/profiles/${profileid}/posts`, {
-          headers: {
-            Authorization: `Bearer ${apiKey}`,
-          },
-        });
+        const postsResponse = await fetch(
+          `https://api.noroff.dev/api/v1/social/profiles/${profileid}/posts`,
+          {
+            headers: {
+              Authorization: `Bearer ${apiKey}`,
+            },
+          }
+        );
         const postsData = await postsResponse.json();
         setPosts(postsData);
       } catch (error) {
@@ -32,22 +38,124 @@ export default function ProfileDetail() {
     fetchProfile();
   }, [profileid, apiKey]);
 
+  const handleEditPost = (post) => {
+    // Open the edit form or modal and populate it with the post data.
+    setEditingPost(post);
+  };
+
+  const handleUpdatePost = (updatedPost) => {
+    // Handle the PUT request to update the post.
+    fetch(`https://api.noroff.dev/api/v1/social/posts/${editingPost.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(updatedPost),
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Post updated successfully.
+          // You may want to update the local state or re-fetch posts here if needed.
+          setEditingPost(null); // Close the edit form or modal.
+        } else {
+          // Handle error if the update request is not successful.
+        }
+      })
+      .catch((error) => {
+        console.error("Error updating post:", error);
+      });
+  };
+
+  const handleDeletePost = (postId) => {
+    // Handle the DELETE request to delete the post.
+    fetch(`https://api.noroff.dev/api/v1/social/posts/${postId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          // Post deleted successfully.
+          // You may want to update the local state or re-fetch posts here if needed.
+        } else {
+          // Handle error if the delete request is not successful.
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting post:", error);
+      });
+  };
+
   return (
-    <div>
-      <h1>Profile Details</h1>
+    <div className="p-4 bg-white rounded-lg shadow-lg">
+      <h1 className="text-2xl font-semibold mb-4">Profile Details</h1>
       {profile && (
         <div>
-          <h2>{profile.name}</h2>
-          <h3>{profile.email}</h3>
-          <img src={profile.avatar} alt={profile.name} />
+          <h2 className="text-xl font-semibold mb-2">{profile.name}</h2>
+          <h3 className="text-lg text-gray-600 mb-4">{profile.email}</h3>
+          <img
+            src={profile.avatar}
+            alt={profile.name}
+            className="w-16 h-16 rounded-full mb-4"
+          />
         </div>
       )}
-      <h1>Posts</h1>
+      <h1 className="text-2xl font-semibold mt-8 mb-4">Posts</h1>
       <ul>
         {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
+          <li key={post.id} className="mb-4">
+            <div>
+              <h3 className="text-xl font-semibold">{post.title}</h3>
+              <p className="text-gray-600">{post.content}</p>
+              <button
+                onClick={() => handleEditPost(post)}
+                className="bg-blue-500 text-white px-4 py-2 rounded-md mt-2"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => handleDeletePost(post.id)}
+                className="bg-red-500 text-white px-4 py-2 rounded-md mt-2"
+              >
+                Delete
+              </button>
+            </div>
+          </li>
         ))}
       </ul>
+      {editingPost && (
+        <div>
+          <h2 className="text-xl font-semibold mt-8 mb-4">Edit Post</h2>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleUpdatePost({
+                ...editingPost,
+                title: e.target.elements.title.value,
+              });
+            }}
+          >
+            <input
+              type="text"
+              name="title"
+              value={editingPost.title}
+              onChange={(e) => {
+                const updatedTitle = e.target.value;
+                setEditingPost({ ...editingPost, title: updatedTitle });
+              }}
+              className="w-full py-2 px-3 rounded-md border border-gray-300 focus:ring focus:ring-blue-500"
+            />
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+            >
+              Save
+            </button>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
